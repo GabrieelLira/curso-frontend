cb6a60e62c4dfa2449d5c374c6253265c2e0a894
Gabriel Lira <GabrieLira.contato@outlook.com>
2022-08-14 05:12:13 -0300

    Módulo 17

--- html-css-js/17-jQuerry/js/custom.js
+++ html-css-js/17-jQuerry/js/custom.js
@@ -12,46 +12,80 @@
     
         console.log(titulos.first());
     
-        // Configuração de produtos
-    
+       
+        // Destaque escrito Novo ao lado do produto
         $('.featured-item a').addClass('btn btn-dark stretch-link');
+        $('.featured-item:first h4').append('<span class="badge bg-secondary" style="font-size:10px;">Novo</span>')
+        // fim
+
+
+        /* 
+        $('.featured-item a').on('blur', function(event){
+
+        event.preventDefault();
+
+        alert('Produto esgotado');
+
+        })
+        */
+
+
+        // Abrir o modal de contato e sobre
+        $('.nav-modal-open').on('click', function(e){
+        e.preventDefault();
+        let elem = $(this).attr('rel')
+        $('.modal-body').html($('#'+elem).html())
+        $('.modal-header h5.modal-title').html($(this).text())
+        let myModal = new bootstrap.Modal($('#modelId'))
+        myModal.show()
+        })
+        //fim
+
+
+        // Validação de campos
+        function validate( elem ){
+        if(elem.val() == '') {
+            elem.parent().find('.text-muted').show()
+            elem.addClass('invalid')
+            return false
+        } else {
+            elem.parent().find('.text-muted').hide()
+            elem.removeClass('invalid')
+        }
+        }
     
-        $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
-        // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
-        // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
-        // $('.featured-item:first h4').addClass('active')
-        // $('.featured-item:first h4').removeClass('active')
-        // $('.featured-item:first h4').toggleClass('active')
-        // $('.featured-item:first h4').hide()
-        // $('.featured-item:first h4').show()
-        // $('.featured-item:first h4').fadeIn(2000)
-        // $('.featured-item:first h4').fadeOut()
-        //  $('.featured-item:first h4').css('color', '#f00')
-         
-         $('.featured-item h4').dblclick( function(){
-    
-            $(this).css({
-                'color': '#f00',
-                'background': '#ff0',
-                'font-weight': '100',
-            });
-    
-         });
-    
-         /*
-          * Manipulação de eventos
-          */
-         $('.featured-item a').on('blur', function(event){
-    
-            event.preventDefault();
-    
-            alert('Produto esgotado');
-    
-         })
-         
-         
-         
-         
+        $('body').on('submit', '.modal-body .form', function(e){
+        e.preventDefault()
+
+        const inputName = $('#nome')
+        const inputEmail = $('#email')
+
+        validate(inputName)
+        validate(inputEmail)
+        
+        if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+            return false
+        } else {
+            $(this).submit()
+        }
+        })
+
+        $('body').on('blur', '#nome', function(){
+        validate($(this));
+        })
+
+        $('body').on('blur', '#email', function(){
+        validate($(this));
+        })
+
+        $('body').on('blur', '#phone', function(){
+        validate($(this))
+        $(this).mask('(00) 00000-0000');
+        })
+        //fim
+
+
+
     
     
     
