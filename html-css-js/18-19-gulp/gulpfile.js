2ca61930426134af8b6ce8e114409c4abf4f7f8a
Gabriel Lira <GabrieLira.contato@outlook.com>
2022-08-18 05:23:26 -0300

    Módulo 19

--- html-css-js/18-19-gulp/gulpfile.js
+++ html-css-js/18-19-gulp/gulpfile.js
@@ -1,13 +1,19 @@
+const { series, parallel } = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
+
 
 function tarefasCSS(cb) {
 
-    return gulp.src([
+    gulp.src([
     './vendor/bootstrap/css/bootstrap.min.css',
     './vendor/owl/css/owl.css',
     './vendor/fonts/*.css',
@@ -18,12 +24,12 @@ function tarefasCSS(cb) {
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min'})) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
-
+    return cb()
 }
 
-function tarefasJS(){
+function tarefasJS(cb){
 
-    return gulp.src([
+    gulp.src([
     './vendor/jquery/jquery-3.6.0.min.js',
     './vendor/bootstrap/js/bootstrap.min.js',
     './vendor/owl/js/owl.js',
@@ -31,20 +37,49 @@ function tarefasJS(){
     './vendor/jquery-ui/jquery-ui.min.js',
     'src/js/custom.js'
 ])
+        .pipe(babel({
+            comments: false,
+        }))
         .pipe(concat('libs.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min'})) //libs.min.js
         .pipe(gulp.dest('./dist/js'))
+    return cb()
 }
 
 
-function tarefasImagem(){
+function tarefasImagem(cb){
     
-    return gulp.src('./src/images/*')
-    .pipe(image())
-    .pipe(gulp.dest('./dist/images'))
+    gulp.src('./src/images/*')
+        .pipe(image())
+        .pipe(gulp.dest('./dist/images'))
+    return cb()
 }
+
+
+function tarefasHTML(cb){
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+    return cb()
+}
+
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    })
+    gulp.watch('./src/**/*').on('change', process)
+    gulp.watch('./src/**/*').on('change', reload)
+})
+
+
+const process = series( tarefasHTML, tarefasJS, tarefasCSS )
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+
+exports.default = process
