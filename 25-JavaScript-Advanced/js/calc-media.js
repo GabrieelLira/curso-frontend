fcd13d5a2f618504bc8b6cf17a5d96e12e7431ea
Gabriel Lira <GabrieLira.contato@outlook.com>
2022-09-15 04:27:28 -0300

    Módulo 25
    Modificações no código

--- 25-JavaScript-Advanced/js/calc-media.js
+++ 25-JavaScript-Advanced/js/calc-media.js
@@ -1,114 +1,75 @@
-/**
- * fiz essas const para não ter que ficar puxando esses key toda vez que eu ia fazer uma validação
- */
-const res = document.getElementById('resultado')
-const n1 = document.getElementById('n1')
-const n2 = document.getElementById('n2')
-const n3 = document.getElementById('n3')
-const n4 = document.getElementById('n4')
-
-
 class Calculo {
     constructor() {
         this.arrayNumeros = [];
     }
-    enviar() {
-        let numeros = this.dados();
-        this.adicionar(numeros)
-        this.resultado()
-        if(this.validaDados(numeros)){
-            this.adicionar(numeros)
-        }
-        if(this.validaDados2(numeros)){
-            this.adicionar(numeros)
-          
-        }
-        if(this.validaDados3(numeros)){
-            this.adicionar(numeros)
-            
-        }
-        if(this.validaDados4(numeros)){
-            this.adicionar(numeros)
-        }
+
+    enviar(){ 
+        let numeros = this.dados(); 
+        this.adicionar(numeros); 
+        this.resultado() 
     }
-
-
-    /**
-     * Função para calcular o resultado
-     */
-    resultado(){
+ 
+    // Função para calcular o resultado
+    resultado(){ 
         let res = document.getElementById('resultado')
-        for (let i = 0; i < this.arrayNumeros.length; i++){
-            let soma = Number(this.arrayNumeros[i].n1) + Number(this.arrayNumeros[i].n2) + Number(this.arrayNumeros[i].n3) + Number(this.arrayNumeros[i].n4) 
-            res.innerText = soma / 4
+        res.innerText = ''
+        if(this.dados() === false){
+            document.getElementById('formulario-01').style = 'border: 1px solid red'
+            document.querySelector('.mensagem').innerHTML = "Verifique o preenchimento dos campos";
+            res.innerText = ''
+        } else{
+            document.getElementById('formulario-01').style = 'border: '
+            for (let i = 0; i < this.arrayNumeros.length; i++){
+                let soma = Number(this.arrayNumeros[i].n1) + Number(this.arrayNumeros[i].n2) + Number(this.arrayNumeros[i].n3) + Number(this.arrayNumeros[i].n4) 
+                let media = soma / 4
+                let condicao = media >= 8 ? "aprovado" : "reprovado";
+                document.querySelector('.mensagem').innerHTML = "";
+                res.innerText = `Média: ${media}  / Resultado: ${condicao}` 
+            }
         }
     }  
-    
+     
+     
+    //Função para dar push nos campos
+    adicionar(numeros){ this.arrayNumeros.push(numeros) } 
 
-    /**
-     * Função para dar push nos campos
-     */
-    adicionar(numeros){ 
-        this.arrayNumeros.push(numeros)
-    }
-
-
-    /**
-     * Não consegui fazer a validação dos campos de outro jeito a não ser puxar uma por uma
-     */
-    validaDados(numeros){
-        if(numeros.n1 == '' || numeros.n1 > 10 || numeros.n1 < 0){
-        n1.style = 'border: 1px solid red'
-        res.innerHTML = '';
-        return false
-        }
-        else{
-            n1.style = 'border: 1px solid black'
-        return true
-    }}
-    validaDados2(numeros){
-        if(numeros.n2 == '' || numeros.n2 > 10 || numeros.n2 < 0){
-        n2.style = 'border: 1px solid red'
-        res.innerHTML = '';
-        return false
-        }
-        else{
-            n2.style = 'border: 1px solid black'
-        return true
-    }}
-    validaDados3(numeros){
-        if(numeros.n3 == '' || numeros.n3 > 10 || numeros.n3 < 0){
-        n3.style = 'border: 1px solid red'
-        res.innerHTML = '';
-        return false
-        }
-        else{
-            n3.style = 'border: 1px solid black'
-        return true
-    }}
-    validaDados4(numeros){
-        if(numeros.n4 == '' || numeros.n4 > 10 || numeros.n4 < 0){
-        n4.style = 'border: 1px solid red'
-        res.innerHTML = '';
-        return false
-        }
-        else{
-            n4.style = 'border: 1px solid black'
-        return true
-    }}
-    
-    /**
-     * puxando os value para fazer um array
-     */
-    dados(){
-        let numeros = {}
+    //puxando os value para fazer um array
+    dados(){   
+        let numeros = []
         numeros.resultado = this.resultado;
         numeros.n1 = document.getElementById('n1').value;
         numeros.n2 = document.getElementById('n2').value;
         numeros.n3 = document.getElementById('n3').value;
         numeros.n4 = document.getElementById('n4').value;
-        return numeros;
+        for (let i in numeros){ 
+            if(numeros[i] == '' || numeros[i] > 10 || numeros[i] < 0){
+        
+                return false
+            } 
+        } return numeros;
     }
 }
+ 
+ var calculo = new Calculo();
 
-var calculo = new Calculo();
+ function validaCampoNumerico(elemento){
+    elemento.addEventListener('focusout', function(event) {
+        event.preventDefault();
+        let numero = this.value.match(/^[\d]5-[\d]3/) ? this.value.replace(/-/, "") : this.value; 
+        if(numero != "" && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10){
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
+let camposNumericos = document.querySelectorAll('input.numeros');
+for( let emFoco of camposNumericos) {
+    validaCampoNumerico(emFoco);
+}
